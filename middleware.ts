020025e171ea0bb30ec4bcfef233b082c31b6c4d
Valentin Madrid <valentin@incility.com>
2023-08-17 16:10:28 +0200

    update middleware to expose pathname

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,18 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
+  // Create supabase client and get session
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
   await supabase.auth.getSession();
-  return res;
+
+  // Set the "x-pathname" header
+  const requestHeaders = new Headers(req.headers);
+  requestHeaders.set("x-pathname", req.nextUrl.pathname);
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
 }
