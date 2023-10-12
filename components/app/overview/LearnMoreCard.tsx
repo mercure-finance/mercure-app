b68c861db5c485d2d857fc40c05ece6e5b1fecd0
Valentin Madrid <valentin@incility.com>
2023-10-13 00:40:37 +0200

    push

--- components/app/overview/LearnMoreCard.tsx
+++ components/app/overview/LearnMoreCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,24 +9,33 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { PiggyBankIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-const LearnMore = () => (
-  <Card className="mt-4 bg-indigo-600">
-    <CardHeader>
-      <div className="flex items-center">
-        <div className="rounded-full bg-white items-center justify-center p-3">
-          <PiggyBankIcon className="left-0" />
+const LearnMore = () => {
+  const router = useRouter();
+  return (
+    <Card className="mt-4 bg-indigo-600">
+      <CardHeader>
+        <div className="flex items-center">
+          <div className="rounded-full bg-white items-center justify-center p-3">
+            <PiggyBankIcon className="left-0" />
+          </div>
+          <CardTitle className="text-white ml-2">Learn More</CardTitle>
         </div>
-        <CardTitle className="text-white ml-2">Learn More</CardTitle>
-      </div>
-      <CardDescription className="text-slate-100">
-        Want to learn how Mercure works ?
-      </CardDescription>
-      <div className="">
-        <Button variant="secondary">Take me there</Button>
-      </div>
-    </CardHeader>
-  </Card>
-);
+        <CardDescription className="text-slate-100">
+          Want to learn how Mercure works ?
+        </CardDescription>
+        <div className="">
+          <Button
+            variant="secondary"
+            onClick={() => router.push("https://twitter.com/MercureFinance")}
+          >
+            Take me there
+          </Button>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+};
 
 export default LearnMore;
